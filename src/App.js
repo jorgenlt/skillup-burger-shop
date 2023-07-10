30445e058b9bca9b721ba744ea8edf60093d935b
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-07-10 16:03:06 +0200

    first draft

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { useState, useEffect} from 'react'
 import Home from "./components/home/Home";
 import Footer from "./components/layout/Footer";
 import Header from "./components/layout/Header";
 import Contact from "./components/contact/Contact";
-// Add module imports for CART
-
-// Add module imports for Shipping
+import Cart from "./components/cart/Cart";
+import Shipping from "./components/cart/Shipping";
 
 import Login from "./components/login/Login";
 import Profile from "./components/profile/Profile";
-// Add module imports for MY ORDERS
-
+import MyOrders from './components/myOrders/MyOrders'
 import OrderDetails from "./components/myOrders/OrderDetails";
 import About from "./components/about/About";
 
@@ -31,6 +30,55 @@ import "./styles/orderDetails.scss";
 import "./styles/about.scss";
 
 function App() {
+  const [cheeseBurger, setCheeseBurger] = useState(0);
+  const [vegCheeseBurger, setVegCheeseBurger] = useState(0);
+  const [cheeseBurgerFries, setCheeseBurgerFries] = useState(0);
+  const [total, setTotal] = useState(0);
+
+  const increment = item => {
+    switch (item) {
+      case 'cheeseBurger':
+        setCheeseBurger(prev => prev + 1)
+        break;
+      case 'vegCheeseBurger':
+        setVegCheeseBurger(prev => prev + 1)
+        break;
+      case 'cheeseBurgerFries':
+        setCheeseBurgerFries(prev => prev + 1)
+        break;
+      default:
+        console.log('Unknown item.');
+    }
+  }
+
+  const decrement = item => {
+    switch (item) {
+      case 'cheeseBurger':
+        cheeseBurger !== 0 ? setCheeseBurger(prev => prev - 1) : setCheeseBurger(0);
+        break;
+      case 'vegCheeseBurger':
+        vegCheeseBurger !== 0 ? setVegCheeseBurger(prev => prev - 1) : setVegCheeseBurger(0);
+        break;
+      case 'cheeseBurgerFries':
+        cheeseBurgerFries !== 0 ? setCheeseBurgerFries(prev => prev - 1) : setCheeseBurgerFries(0);
+        break;
+      default:
+        console.log('Unknown item.');
+    }
+  }
+
+  useEffect(() => {
+    const cheeseBurgerTotal = 400 * cheeseBurger;
+    const vegCheeseBurgerTotal = 500 * vegCheeseBurger;
+    const cheeseBurgerFriesTotal = 600 * cheeseBurgerFries;
+
+    const cheeseBurgersTotal = cheeseBurgerTotal + vegCheeseBurgerTotal + cheeseBurgerFriesTotal;
+
+    setTotal(cheeseBurgersTotal);
+
+  }, [cheeseBurger, vegCheeseBurger, cheeseBurgerFries])
+
+
   return (
     <Router>
       <Header isAuthenticated={true} />
@@ -38,19 +86,22 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="/about" element={<About />} />
-          //           Add the Route for CART
-      
-          //           Add the Route for SHIPPING
-        
+        <Route path="/cart" element={
+          <Cart 
+            increment={increment}
+            decrement={decrement}
+            cheeseBurger={cheeseBurger}
+            vegCheeseBurger={vegCheeseBurger}
+            cheeseBurgerFries={cheeseBurgerFries}
+            total={total}
+          />
+        } 
+        />
+        <Route path="/shipping" element={<Shipping />} />
+        <Route path="/myorders" element={<MyOrders />} />
         <Route path="/login" element={<Login />} />
         <Route path="/me" element={<Profile />} />
-         //           Add the Route for MY ORDERS
-
         <Route path="/order/:id" element={<OrderDetails />} />
-      
-       
-
-        
       </Routes>
 
       <Footer />
--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -1,75 +1,72 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import burger1 from "../../assets/burger1.png";
-import burger2 from "../../assets/burger2.png";
-// import burger3 here
+import React, { useState, useEffect } from "react"; 
+import { Link } from "react-router-dom"; 
+import burger1 from "../../assets/burger1.png"; 
+import burger2 from "../../assets/burger2.png"; 
+import burger3 from "../../assets/burger3.png"; 
 
-const CartItem = ({ value, title, img, increment, decrement }) => (
-  <div className="cartItem">
-    <div>
-      <h4>{title}</h4>
-      <img src={img} alt="Item" />
-    </div>
+const CartItem = ({ value, title, img, increment, decrement }) => ( 
+  <div className="cartItem"> 
+    <div> 
+      <h4>{title}</h4> 
+      <img src={img} alt="Item" /> 
+    </div> 
+    
+    <div> 
+      <button onClick={decrement}>-</button> 
+      <input type="number" readOnly value={value} /> 
+      <button onClick={increment}>+</button> 
+    </div> 
+  </div> 
+); 
+  
+const Cart = ({ increment, decrement, cheeseBurger, vegCheeseBurger, cheeseBurgerFries, total }) => { 
 
-    <div>
-      <button onClick={decrement}>-</button>
-      <input type="number" readOnly value={value} />
-      <button onClick={increment}>+</button>
-    </div>
-  </div>
-);
-
-const Cart = () => {
-  const increment = (item) => {};
-
-  const decrement = (item) => {};
-
-  return (
-    <section className="cart">
-      <main>
-        <CartItem
-          title={"Cheese Burger"}
-          img={burger1}
-          value={0}
-          increment={() => increment(1)}
-
-        // Add the function for decrementing the order by 1 
-       
-        />
-        <CartItem
-          title={"Veg Cheese Burger"}
-          img={burger2}
-          value={0}
-          increment={() => increment(2)}
-        // Add the function for decrementing the order by 2
-       
-        />
-
-        {/* Fill up the code for Cheese Burger similarly */}
-       
-
-        <article>
-          <div>
-            <h4>Sub Total</h4>
-            <p>₹{2000}</p>
-          </div>
-          <div>
-            <h4>Tax</h4>
-            <p>₹{2000 * 0.18}</p>
-          </div>
-          <div>
-            <h4>Shipping Charges</h4>
-            <p>₹{200}</p>
-          </div>{" "}
-          <div>
-            <h4>Total</h4>
-            <p>₹{2000 + 2000 * 0.18 + 200}</p>
-          </div>
-          <Link to="/shipping">Checkout</Link>
-        </article>
-      </main>
-    </section>
-  );
+  return ( 
+    <section className="cart"> 
+      <main> 
+          <CartItem 
+          title={"Cheese Burger"} 
+          img={burger1} 
+          value={cheeseBurger} 
+          increment={() => increment('cheeseBurger')} 
+          decrement={() => decrement('cheeseBurger')} 
+        /> 
+        <CartItem 
+          title={"Veg Cheese Burger"} 
+          img={burger2} 
+          value={vegCheeseBurger} 
+          increment={() => increment('vegCheeseBurger')} 
+          decrement={() => decrement('vegCheeseBurger')} 
+        /> 
+        <CartItem 
+          title={"Cheese Burger with French Fries"} 
+          img={burger3} 
+          value={cheeseBurgerFries} 
+          increment={() => increment('cheeseBurgerFries')} 
+          decrement={() => decrement('cheeseBurgerFries')}
+        />  
+        <article> 
+          <div> 
+            <h4>Sub Total</h4> 
+            <p>₹{total}</p> 
+          </div> 
+          <div> 
+            <h4>Tax</h4> 
+            <p>₹{total * 0.18}</p> 
+          </div> 
+          <div> 
+            <h4>Shipping Charges</h4> 
+            <p>₹{200}</p> 
+          </div>{" "} 
+          <div> 
+            <h4>Total</h4> 
+            <p>₹{total * 1.18 + 200}</p> 
+          </div> 
+          <Link to="/shipping">Checkout</Link> 
+        </article> 
+      </main> 
+    </section> 
+  ); 
 };
 
 export default Cart;
